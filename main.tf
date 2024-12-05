b33a8716cd03a1093d8dae84892d99889b95cf75
vitorrafael <vrsilveira07@gmail.com>
2024-12-05 19:52:11 -0300

    test: test main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
 terraform {
-  backend "remote" {
-    organization = "tech-challenge-soat-8-group-6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "FIAP-SOAT-8-G6"
+
+    workspaces {
+      name = "gh-actions"
+    }
   }
 }
-
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
